19dd0fc4998f7f536b899686a24b59f42c9c5cb9
Luka Vashakmadze <50746021+Vashakmadze@users.noreply.github.com>
2023-02-28 11:25:30 +0100

    changed navigation urls

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,19 +4,19 @@ const Header = () => (
     <header className="text-right">
         <nav className="flex items-center justify-between flex-col sm:flex-row flex-wrap">
             <div className="flex items-center flex-shrink-0 text-white">
-                <Link to="/">
+                <Link to="/jobs-api/">
                     <span className="font-semibold text-[3rem] tracking-tight mb-[2rem] sm:mb-0 transition duration-300 ease-in-out hover:text-teal-200">Jobs API</span>
                 </Link>
             </div>
             <div className="lg:flex lg:items-center lg:w-auto">
                 <div className="lg:flex-grow text-lg">
-                    <Link to="/docs" className="sm:text-[1.25rem] transition duration-300 ease-in-out mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+                    <Link to="/jobs-api/docs" className="sm:text-[1.25rem] transition duration-300 ease-in-out mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
                         DOCS
                     </Link>
-                    <Link to="/examples" className="sm:text-[1.25rem] transition duration-300 ease-in-out mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white ml-4">
+                    <Link to="/jobs-api/examples" className="sm:text-[1.25rem] transition duration-300 ease-in-out mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white ml-4">
                         EXAMPLES
                     </Link>
-                    <Link to="/register" className="sm:text-[1.25rem] transition duration-300 ease-in-out mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white ml-4">
+                    <Link to="/jobs-api/register" className="sm:text-[1.25rem] transition duration-300 ease-in-out mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white ml-4">
                         REGISTER
                     </Link>
                 </div>
