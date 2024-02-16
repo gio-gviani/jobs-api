7f135c455db79b3b37e9477ca3ea2a6f5e03628e
luka.vashakmadze <geogamerstime@gmail.com>
2024-02-16 14:35:01 +0100

    refactored index, removed comments

--- server/index.js
+++ server/index.js
@@ -8,69 +8,40 @@ const API = require("./middleware/apikeys")
 const fetchMainPage = require('./fetchBody')
 const fetchJobs = require("./fetchJobs")
 const port = process.env.PORT || 5000;
-const url = 'https://jobs.ge/en/';  
-const api = require("./middleware/apikeys");
+const url = 'https://jobs.ge/en/';
+const deleteCollections = require("./mongdodb/getDatabaseJobs");
 
-// host validation from the github repo 
-// error 404 page
 const app = express();
 app.use(express.json());
-app.use(cors());
-
-let jobs = []
-let techJobs = []
-let management = []
-let finance = []
-let logistics = []
-let construction = []
-let law = []
-let medicine = []
-let security = []
-
+app.use(cors(
+    {
+        origin: "*"
+    }
+));
 
 // scrape data function to get the data from the webpage - not working atm
-
 const scrapeData = async () => {
-    jobs = await fetchJobs(url, jobs, "all");
-    techJobs = await fetchJobs(`${url}?cid=6`, techJobs, "tech");
-    management = await fetchJobs(`${url}?cid=1`, management, "management");
-    finance = await fetchJobs(`${url}?cid=3`, finance, "finance");
-    logistics = await fetchJobs(`${url}?cid=5`, logistics, "logistics");
-    construction = await fetchJobs(`${url}?cid=11`, construction, "construction"); // construction repari 11
-    law = await fetchJobs(`${url}?cid=7`, law, "law"); // law 7
-} 
-
-// end
- 
-const getDataFromDatabase = async () => {
-    jobs = await getData("all");
-    techJobs = await getData("tech");
-    management = await getData("management");
-    finance = await getData("finance");
-    logistics = await getData("logistics");
-    construction = await getData("construction");
-    law = await getData("law");
+    await fetchJobs(url, "all");
+    console.log("\u001b[1;32m finished all fetch")
+    await fetchJobs(`${url}?cid=6`, "tech");
+    console.log("\u001b[1;32m finished tech fetch")
+    await fetchJobs(`${url}?cid=1`, "management");
+    console.log("\u001b[1;32m finished management fetch")
+    await fetchJobs(`${url}?cid=3`, "finance");
+    console.log("\u001b[1;32m finished finance fetch")
+    await fetchJobs(`${url}?cid=5`, "logistics");
+    console.log("\u001b[1;32m finished logistics fetch")
+    await fetchJobs(`${url}?cid=11`, "construction"); // construction repari 11
+    console.log("\u001b[1;32m finished construction fetch")
+    fetchJobs(`${url}?cid=7`, "law"); // law 7
+    console.log("\u001b[1;32m finished law fetch")
 }
 
-getDataFromDatabase()
-
-
-// Cron schedule to fetch new data every day - soon to be implemented
-
-// cron.schedule('0 0 * * *', () => { // function reruns every day
-//     fetchJobs(url, jobs); 
-//     // fetchJobs(`${url}?cid=8`, medicine); // medicine 8 
-//     fetchJobs(`${url}?cid=6`, techJobs);
-//     fetchJobs(`${url}?cid=1`, management);
-//     fetchJobs(`${url}?cid=3`, finance);
-//     fetchJobs(`${url}?cid=5`, logistics);
-//     fetchJobs(`${url}?cid=11`, construction); // construction repari 11
-//     fetchJobs(`${url}?cid=7`, law); // law 7
-// });
-
-// end
-
-// Rest of the data fetching - Soon to be implemented 
+// Cron schedule to fetch new data every month
+cron.schedule('0 0 1 * *', () => {
+    deleteCollections();
+    scrapeData();
+});
 
 // fetchJobs(`${url}?cid=17`, security); // security safety 17
 // fetchJobs(`${url}?cid=2`, techJobs); // sales procurement 2
@@ -85,8 +56,6 @@ getDataFromDatabase()
 // fetchJobs(`${url}?cid=17`, security); // security safety 17
 // fetchJobs(`${url}?cid=8`, medicine); // medicine 8 
 
-// End 
-
 app.get('/', (req, res) => {
     res.status(200).send({ data: { message: 'Everything works fine.' } });
 });
@@ -94,42 +63,19 @@ app.get('/', (req, res) => {
 app.post("/jobs/register", async (req, res) => {
     let email = req.body.email;
     let user = await API.createUser(email, req);
-    res.status(201).send({success: true, data: user});
+    res.status(201).send({ success: true, data: user });
 })
 
-app.get("/jobs/:cat", API.validateKey, (req, res) => {
+app.get("/jobs/:cat", API.validateKey, async (req, res) => {
     const cat = req.params.cat;
-    let data = {request: "success", data: "no data found or no category."};
-    switch(cat) {
-        case "tech":
-            data = techJobs;
-            break;
-        case "all": 
-            data = jobs;
-            break;
-        case "management":
-            data = management;
-            break;
-        case "finance": 
-            data = finance;
-            break;
-        case "logistics":
-            data = logistics
-            break;  
-        case "construction":
-            data = construction
-            break;
-        case "law":
-            data = law;
-            break;
-    }
-    
+    let data = { request: "success", data: "no data found or no category." };
+    data = await getData(cat);
     res.status(200).send(JSON.stringify(data));
 })
 
 app.get('*', API.validateKey, (req, res) => {
-    res.send({error: "No routes found"});
-  })
+    res.send({ error: "No routes found" });
+})
 
 const startServer = async () => {
     try {
